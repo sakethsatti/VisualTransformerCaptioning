fd18666bb4cb5f60138503f934963596f55461f3
sakethsatti <saksatti@gmail.com>
2023-11-25 13:08:16 -0500

    remove training=training

--- TransformerCode/decoder_layer.py
+++ TransformerCode/decoder_layer.py
@@ -18,16 +18,25 @@ class DecoderLayer(nn.Module):
         self.dropout3 = nn.Dropout(p = rate)
 
     def call(self, x, enc_output, training,look_ahead_mask=None, padding_mask=None):
+        if training:
+            self.dropout1.train()
+            self.dropout2.train()
+            self.dropout3.train()
+        else:
+            self.dropout1.eval()
+            self.dropout2.eval()
+            self.dropout3.eval()
+        
         attn1, attn_weights_block1 = self.mha1(x, x, x, look_ahead_mask)  # (batch_size, target_seq_len, d_model)
-        attn1 = self.dropout1(attn1, training=training)
+        attn1 = self.dropout1(attn1)
         out1 = self.layernorm1(attn1 + x)
 
         attn2, attn_weights_block2 = self.mha2(enc_output, enc_output, out1, padding_mask) 
-        attn2 = self.dropout2(attn2, training=training)
+        attn2 = self.dropout2(attn2)
         out2 = self.layernorm2(attn2 + out1)  # (batch_size, target_seq_len, d_model)
 
         ffn_output = self.ffn(out2)  # (batch_size, target_seq_len, d_model)
-        ffn_output = self.dropout3(ffn_output, training=training)
+        ffn_output = self.dropout3(ffn_output)
         out3 = self.layernorm3(ffn_output + out2)  # (batch_size, target_seq_len, d_model)
 
         return out3, attn_weights_block1, attn_weights_block2
--- TransformerCode/encoder_layer.py
+++ TransformerCode/encoder_layer.py
@@ -14,12 +14,19 @@ class EncoderLayer(nn.Module):
         self.dropout2 = nn.Dropout(p = rate)
 
     def call(self, x, training, mask=None):
+        if training:
+            self.dropout1.train()
+            self.dropout2.train()
+        else:
+            self.dropout1.eval()
+            self.dropout2.eval()
+       
         attn_output, _ = self.mha(x, x, x, mask)  # (batch_size, input_seq_len, d_model)
-        attn_output = self.dropout1(attn_output, training=training)
+        attn_output = self.dropout1(attn_output)
         out1 = self.layernorm1(x + attn_output)  # (batch_size, input_seq_len, d_model)
 
         ffn_output = self.ffn(out1)  # (batch_size, input_seq_len, d_model)
-        ffn_output = self.dropout2(ffn_output, training=training)
+        ffn_output = self.dropout2(ffn_output)
         out2 = self.layernorm2(out1 + ffn_output)  # (batch_size, input_seq_len, d_model)
         return out2
 
