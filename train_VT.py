8482a0c5458e875ae8bf1a5952399049a693755f
saksatti <saksatti@gmail.com>
2023-12-13 10:19:09 -0500

    add layer, increase dimensions and tokens

--- train_VT.py
+++ train_VT.py
@@ -87,7 +87,7 @@ def evaluate(model, data_loader, loss_history):
 
 
 if __name__ == "__main__":
-    model = ViTResNet(BasicBlock, [3, 3, 3])
+    model = ViTResNet(BasicBlock, [2, 3, 3, 3])
     model = model.to(DEVICE_NAME)
     
     #optimizer = torch.optim.Adam(model.parameters(), lr=LR)
